9e1763d9dfb8958e8328b35c4dfc925c9cb0c654
k3ntako <kentarokaneki@gmail.com>
2019-11-07 15:43:39 -0500

    Add couple functions to Game Class

--- tests/1_GameClass.test.ts
+++ tests/1_GameClass.test.ts
@@ -22,24 +22,6 @@ describe('Fundamentals', (): void => {
       assert.strictEqual(game.getElixir(), 0);
     });
 
-    it('should have a method called addOne', (): void => {
-      const game = new Game();
-      assert.strictEqual(typeof game.addOne, 'function');
-    });
-
-    it('should have a method called addOne that adds 1 to elixir field', (): void => {
-      const game = new Game();
-      game.addOne();
-      assert.strictEqual(game.elixir, 1);
-    });
-
-    it('should have a method called addOne that adds 2 to elixir field if called twice', (): void => {
-      const game = new Game();
-      game.addOne();
-      game.addOne();
-      assert.strictEqual(game.getElixir(), 2);
-    });
-
     it('should have a method called setElixir', (): void => {
       const game = new Game();
       assert.strictEqual(typeof game.setElixir, 'function');
@@ -51,5 +33,60 @@ describe('Fundamentals', (): void => {
       assert.strictEqual(game.getElixir(), 5);
     });
 
+    it('should have a method called addElixir', (): void => {
+      const game = new Game();
+      assert.strictEqual(typeof game.addElixir, 'function');
+    });
+
+    it('should have a method called addElixir that adds the argument to the elixir field', (): void => {
+      const game = new Game();
+      game.addElixir(3);
+      assert.strictEqual(game.getElixir(), 3);
+    });
+
+    it('addElixir should never allow elixir count to be more than 10', () => {
+      const game = new Game();
+      game.setElixir(9);
+      game.addElixir(2);
+      assert.isAtMost(game.getElixir(), 10);
+    });
+
+    it('if the total is greater than 10, addElixir should set the field to 10', () => {
+      const game = new Game();
+      game.setElixir(9);
+      game.addElixir(2);
+      assert.strictEqual(game.getElixir(), 10);
+    });
+
+    it('should have a method called subtractElixir', (): void => {
+      const game = new Game();
+      assert.strictEqual(typeof game.subtractElixir, 'function');
+    });
+
+    it('should have a method called subtractElixir that subtracts the argument from the elixir field', (): void => {
+      const game = new Game();
+      game.setElixir(7);
+      game.subtractElixir(3);
+      assert.strictEqual(game.getElixir(), 4);
+    });
+
+    it('subtractElixir should never allow elixir count to be less than 0', (): void => {
+      const game = new Game();
+      game.setElixir(2);
+      game.subtractElixir(3);
+      assert.isAtLeast(game.getElixir(), 0);
+    });
+
+    it('if the result is less than 0, subtractElixir should set the field to 0', () => {
+      const game = new Game();
+      game.setElixir(2);
+      game.subtractElixir(3);
+      assert.strictEqual(game.getElixir(), 0);
+    });
+
+    it('should have a method called playCard', (): void => {
+      const game = new Game();
+      assert.strictEqual(typeof game.playCard, 'function');
+    });
   });
 });
